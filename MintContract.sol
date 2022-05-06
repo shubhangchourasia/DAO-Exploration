dc655a10c5b19dcdfdf88fc0cf568dfc642dfbbb
Shubhang Chourasia <70596665+shubhangchourasia@users.noreply.github.com>
2022-05-06 15:35:13 +0530

    Update MintContract.sol

--- MintContract.sol
+++ MintContract.sol
@@ -5,11 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MintContract is ERC721, Ownable {
-    // uint256 public mintPrice = 0 ether;
-    // uint256 public totalSupply;
-    // uint256 public maxSupply;
-    // bool public isMintEnabled;
-    // mapping(address => uint256) public mintedWallets;
+    uint256 public mintPrice = 0 ether;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    bool public isMintEnabled;
+    mapping(address => uint256) public mintedWallets;
 
     constructor() payable ERC721("Pizza Mint", "PIZZAMINT") {
         maxSupply = 100;
